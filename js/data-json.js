2aa042e282a4161b182b1db0aca540f07bd26909
desya42 <59778932+desya42@users.noreply.github.com>
2020-04-21 00:39:56 -0500

    Add files via upload

--- js/data-json.js
+++ js/data-json.js
@@ -1,15 +1,5 @@
-// NOTE: If you run this file locally
-// You will not get a server status
-// You can comment out lines 9 and 26 to make it work locally
 
-var xhr = new XMLHttpRequest();                 // Create XMLHttpRequest object
-
-xhr.onload = function () {                       // When readystate changes
-  // The following conditional check will not work locally - only on a server
-  //if(xhr.status === 200) {                      // If server status was ok
-  responseObject = JSON.parse(xhr.responseText);
-
-  // BUILD UP STRING WITH NEW CONTENT (could a// NOTE: If you run this file locally
+};// NOTE: If you run this file locally
 // You will not get a server status
 // You can comment out lines 9 and 26 to make it work locally
 
@@ -43,4 +33,12 @@ xhr.send(null);                                 // Send the request
 // When working locally in Firefox, you may see an error saying that the JSON is not well-formed.
 // This is because Firefox is not reading the correct MIME type (and it can safely be ignored).
 
-// If you get it on a server, you may need to se the MIME type for JSON on the server (application/JSON).lso use DOM manipulation)
+// If you get it on a server, you may need to se the MIME type for JSON on the server (application/JSON)
+
+xhr.open('GET', 'data/data.json', true);        // Prepare the request
+xhr.send(null);                                 // Send the request
+
+// When working locally in Firefox, you may see an error saying that the JSON is not well-formed.
+// This is because Firefox is not reading the correct MIME type (and it can safely be ignored).
+
+// If you get it on a server, you may need to se the MIME type for JSON on the server (application/JSON).
